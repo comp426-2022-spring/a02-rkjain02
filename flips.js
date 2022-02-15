d64d768b9c69263c87b8352b29a49c05ce649246
rishab <rkjain02@ad.unc.edu>
2022-02-15 12:09:05 -0500

    flips error fixed

--- flips.js
+++ flips.js
@@ -2,7 +2,6 @@ import { coinFlips, countFlips } from "./modules/coin.mjs";
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2))
-console.log(args)
 const number = args.number || 1;
 
 var flips = coinFlips(number)
